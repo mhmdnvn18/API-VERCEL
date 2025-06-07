8ac6281ec5a890bf3e0b6cbfd6bc68f0c2747b12
Muhammad Novian <mhmdnvn18@gmail.com>
2025-06-07 11:55:10 +0700

    udpate

--- api/predict.js
+++ api/predict.js
@@ -4,19 +4,16 @@ import * as tf from '@tensorflow/tfjs';
 
 export default async function handler(req, res) {
   try {
-    // Gunakan path relatif tanpa 'file://' agar bisa diakses di Vercel
     const model = await tf.loadLayersModel('https://'+req.headers.host+'/model/model.json');
-    // Proses input dan prediksi
     const input = req.body.input;
     
-    // Pastikan input berbentuk [jumlah_sample, 11]
     const inputTensor = tf.tensor(input).reshape([-1, 11]);
     const prediction = model.predict(inputTensor);
-    const predictionData = await prediction.data(); // ambil data sebelum dispose
+    const predictionData = await prediction.data(); // Get data before disposal
     
-    // Bersihkan tensor setelah digunakan
     inputTensor.dispose();
     prediction.dispose();
+    
     res.status(200).json({ prediction: Array.from(predictionData) });
   } catch (error) {
     res.status(500).json({ error: error.message });
