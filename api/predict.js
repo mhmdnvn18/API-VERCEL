485b905020dc9c1ae984bd8cac4c4035af22efe5
Muhammad Novian <mhmdnvn18@gmail.com>
2025-06-07 11:41:41 +0700

    update

--- api/predict.js
+++ api/predict.js
@@ -8,8 +8,14 @@ export default async function handler(req, res) {
     const model = await tf.loadLayersModel('https://'+req.headers.host+'/model/model.json');
     // Proses input dan prediksi
     const input = req.body.input;
-    const inputTensor = Array.isArray(input[0]) ? tf.tensor(input) : tf.tensor([input]);
+    
+    // Pastikan input berbentuk [jumlah_sample, 11]
+    const inputTensor = tf.tensor(input).reshape([-1, 11]);
     const prediction = model.predict(inputTensor);
+    
+    // Bersihkan tensor setelah digunakan
+    inputTensor.dispose();
+    prediction.dispose();
     res.status(200).json({ prediction: Array.from(prediction.dataSync()) });
   } catch (error) {
     res.status(500).json({ error: error.message });
